9f51979433d4e98771496ef530b9a6a531596c43
Jörg Thalheim <joerg@thalheim.io>
2019-11-16 14:35:44 +0000

    kmod-blacklist-ubuntu: 22-1.1ubuntu1 -> 26-1ubuntu1

--- pkgs/os-specific/linux/kmod-blacklist-ubuntu/default.nix
+++ pkgs/os-specific/linux/kmod-blacklist-ubuntu/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, gnugrep, findutils }:
 
 let
-  version = "22-1.1ubuntu1"; # Zesty
+  version = "26-1ubuntu1"; # Eoan
 
 in stdenv.mkDerivation {
   pname = "kmod-blacklist";
@@ -9,7 +9,7 @@ in stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://launchpad.net/ubuntu/+archive/primary/+files/kmod_${version}.debian.tar.xz";
-    sha256 = "1k749g707ccb82l4xmrkp53khl71f57cpj9fzd1qyzrz147fjyhi";
+    sha256 = "1mpkqjq42y4ydln0hlhy89jryavgyrb18yazh5nk3yqjmzzzddva";
   };
 
   installPhase = ''
