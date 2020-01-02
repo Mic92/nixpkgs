e704f08b74a159d4a1383cf099cb0eabd7ca7420
Jörg Thalheim <joerg@thalheim.io>
2020-01-02 19:40:49 +0000

    python3.pkgs.fritzconnection: 0.8.4 -> 1.2.0

--- pkgs/development/python-modules/fritzconnection/default.nix
+++ pkgs/development/python-modules/fritzconnection/default.nix
@@ -1,24 +1,20 @@
-{ stdenv, buildPythonPackage, fetchPypi, lxml, requests, tkinter }:
+{ stdenv, buildPythonPackage, fetchPypi, requests, pytest }:
 
 buildPythonPackage rec {
   pname = "fritzconnection";
-  version = "0.8.4";
+  version = "1.2.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "adc629a48b50700f5478f69436e4b78c8792a9260cc674cccef15ffe68eb0643";
+    sha256 = "1f5nrvadhxbsbzb765hzhlvy1vjzmcabfz1jm1p0l235lpnz8kxm";
   };
 
-  prePatch = ''
-    substituteInPlace fritzconnection/test.py \
-      --replace "from fritzconnection import" "from .fritzconnection import"
-  '';
-
-  propagatedBuildInputs = [ lxml requests tkinter ];
+  propagatedBuildInputs = [ requests ];
+  checkInputs = [ pytest ];
 
   meta = with stdenv.lib; {
     description = "Python-Tool to communicate with the AVM FritzBox using the TR-064 protocol";
-    homepage = https://bitbucket.org/kbr/fritzconnection;
+    homepage = https://github.com/kbr/fritzconnection;
     license = licenses.mit;
     maintainers = with maintainers; [ dotlambda ];
   };
