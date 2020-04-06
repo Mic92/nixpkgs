4a0bbc80527f7cbee1279174f01cb9f50a6c7b20
Jörg Thalheim <joerg@thalheim.io>
2020-04-06 21:48:50 +0000

    libIDL: fix cross-compilation

--- pkgs/desktops/gnome-2/platform/libIDL/default.nix
+++ pkgs/desktops/gnome-2/platform/libIDL/default.nix
@@ -9,7 +9,11 @@ stdenv.mkDerivation rec {
     sha256 = "08129my8s9fbrk0vqvnmx6ph4nid744g5vbwphzkaik51664vln5";
   };
 
-  buildInputs = [ glib gettext ];
+  configureFlags = stdenv.lib.optional (stdenv.hostPlatform != stdenv.buildPlatform) [
+    "libIDL_cv_long_long_format=ll"
+  ];
 
-  nativeBuildInputs = [ flex bison pkgconfig ];
+  nativeBuildInputs = [ flex bison pkgconfig gettext ];
+
+  buildInputs = [ glib ];
 }
