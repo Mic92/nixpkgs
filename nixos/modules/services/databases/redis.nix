9230155abdf56b16e25e833d4650d6c0c00087f0
Jörg Thalheim <joerg@thalheim.io>
2020-05-30 10:32:17 +0100

    redis: fix redis service

--- nixos/modules/services/databases/redis.nix
+++ nixos/modules/services/databases/redis.nix
@@ -12,7 +12,7 @@ let
     ${condOption "bind" cfg.bind}
     ${condOption "unixsocket" cfg.unixSocket}
     daemonize no
-    supervised systemd
+    #supervised systemd
     loglevel ${cfg.logLevel}
     logfile ${cfg.logfile}
     syslog-enabled ${redisBool cfg.syslog}
@@ -241,7 +241,7 @@ in
         StateDirectory = "redis";
         TimeoutStartSec = "infinity";
         TimeoutStopSec = "infinity";
-        Type = "notify";
+        Type = "simple";
         User = "redis";
         Group = "redis";
       };
