76274aaf3c70daa229efa147c5d4e372b723ff93
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-06-08 16:22:05 +0200

    python3Packages.segments: init at 2.1.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5993,6 +5993,8 @@ in {
 
   seabreeze = callPackage ../development/python-modules/seabreeze { };
 
+  segments = callPackage ../development/python-modules/segments { };
+
   selenium = callPackage ../development/python-modules/selenium { };
 
   serpy = callPackage ../development/python-modules/serpy { };
