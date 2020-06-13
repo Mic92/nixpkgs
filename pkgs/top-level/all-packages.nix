91ad0276c7e0182ff51092fc94abff8fd88febd8
Jörg Thalheim <joerg@thalheim.io>
2020-06-13 12:07:48 +0200

    festival: init at 2.5.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1870,6 +1870,8 @@ in
 
   fdroidserver = python3Packages.callPackage ../development/tools/fdroidserver { };
 
+  festival = callPackage ../applications/audio/festival { };
+
   filebench = callPackage ../tools/misc/filebench { };
 
   fileshare = callPackage ../servers/fileshare {};
