8ff914955c6e3cd0c2ef9de8809d6b920cb4109b
Jörg Thalheim <joerg@thalheim.io>
2020-06-13 19:40:30 +0100

    parallel-wavegan: init at 2020-03-22

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5173,6 +5173,8 @@ in {
 
   parsedatetime = callPackage ../development/python-modules/parsedatetime { };
 
+  parallel-wavegan = callPackage ../development/python-modules/parallel-wavegan { };
+
   param = callPackage ../development/python-modules/param { };
 
   paramiko = callPackage ../development/python-modules/paramiko { };
