041d059a043fa77a2e38a70fabb7246c59789793
Jörg Thalheim <joerg@thalheim.io>
2020-06-22 11:03:58 +0100

    python3.pkgs.feedparser: 5.2.1 -> 6.0.1b3
    This enables python37/python38 support

--- pkgs/development/python-modules/feedparser/default.nix
+++ pkgs/development/python-modules/feedparser/default.nix
@@ -1,16 +1,26 @@
 { stdenv
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
+, pythonOlder
+, sgmllib3k
 }:
 
 buildPythonPackage rec {
   pname = "feedparser";
-  version = "5.2.1";
+  version = "6.0.1b3";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "1ycva69bqssalhqg45rbrfipz3l6hmycszy26k0351fhq990c0xx";
+  disabled = pythonOlder "3.5";
+
+  src = fetchFromGitHub {
+    owner = "kurtmckee";
+    repo = "feedparser";
+    rev = "530bb8cfdfaf007917ab65a79e54edf7136bd9f8";
+    sha256 = "0mr1p9a22js4g4sbkpvb5xc5rr6k48hzm777w5w901m8jimcmh7s";
   };
+
+  propagatedBuildInputs = [
+    sgmllib3k
+  ];
 
   # lots of networking failures
   doCheck = false;
@@ -21,5 +31,4 @@ buildPythonPackage rec {
     license = licenses.bsd2;
     maintainers = with maintainers; [ domenkozar ];
   };
-
 }
