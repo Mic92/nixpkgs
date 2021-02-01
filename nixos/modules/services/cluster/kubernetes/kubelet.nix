cfcd9c5c796884b6bae37d2db34a7b5810597816
Jörg Thalheim <joerg@thalheim.io>
2021-02-01 17:40:27 +0100

    kubernetes: set systemd as cgroup driver

--- nixos/modules/services/cluster/kubernetes/kubelet.nix
+++ nixos/modules/services/cluster/kubernetes/kubelet.nix
@@ -309,6 +309,7 @@ in
             --authentication-token-webhook \
             --authentication-token-webhook-cache-ttl="10s" \
             --authorization-mode=Webhook \
+            --cgroup-driver systemd \
             ${optionalString (cfg.clientCaFile != null)
               "--client-ca-file=${cfg.clientCaFile}"} \
             ${optionalString (cfg.clusterDns != "")
