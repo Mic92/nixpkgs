45af46b2b6eaa7144e8af672a6673f086571a15a
Jörg Thalheim <joerg@thalheim.io>
2021-02-10 11:03:38 +0100

    nextcloud: make home world-readable
    users.users makes home only owner-readable.
    This breaks nginx reading static assets from nextcloud's home,
    after a nixos-rebuild that did not restart nextcloud-setup.
    
    Update nixos/modules/services/web-apps/nextcloud.nix
    
    Co-authored-by: Linus Heckemann <git@sphalerite.org>

--- nixos/modules/services/web-apps/nextcloud.nix
+++ nixos/modules/services/web-apps/nextcloud.nix
@@ -523,7 +523,7 @@ in {
           before = [ "phpfpm-nextcloud.service" ];
           path = [ occ ];
           script = ''
-            chmod og+x ${cfg.home}
+            install -m750 --owner nextcloud --group nextcloud -d ${cfg.home}
 
             ${optionalString (c.dbpassFile != null) ''
               if [ ! -r "${c.dbpassFile}" ]; then
@@ -607,7 +607,6 @@ in {
       users.users.nextcloud = {
         home = "${cfg.home}";
         group = "nextcloud";
-        createHome = true;
         isSystemUser = true;
       };
       users.groups.nextcloud.members = [ "nextcloud" config.services.nginx.user ];
