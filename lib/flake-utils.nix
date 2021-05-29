0f23c03b999e87b39618b7e0b3081668ef6f0fce
Jörg Thalheim <joerg@thalheim.io>
2021-05-29 21:46:35 +0200

    flake-utils: transform all inputs not just nixpkgs

--- lib/flake-utils.nix
+++ lib/flake-utils.nix
@@ -1,4 +1,4 @@
-{ nixpkgs, systems }:
+{ systems }:
 rec {
   /* mapDefaultSystems using default supported systems
      Usage in a nix flake:
@@ -7,11 +7,10 @@ rec {
 
          inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-         outputs = { self, nixpkgs }:
-           nixpkgs.lib.mapDefaultSystems (pkgs:
+         outputs = nixpkgs.lib.mapDefaultSystems ({ self, nixpkgs }:
              {
                packages = {
-                 hello = pkgs.hello;
+                 hello = nixpkgs.legacyPackages.hello;
                };
                defaultPackage = self.packages.hello;
              }
@@ -21,8 +20,8 @@ rec {
      Type: mapDefaultSystems :: (attrset -> attrset) -> attrset
 
      Example:
-       nixpkgs.lib.mapDefaultSystems (pkgs: {
-         defaultPackage = packages.hello;
+       nixpkgs.lib.mapDefaultSystems ({self, nixpkgs}: {
+         defaultPackage = inputs.nixpkgs.legacyPackages.hello;
        })
        => {
          defaultPackage = {
@@ -35,21 +34,24 @@ rec {
   */
   mapDefaultSystems = mapSystems systems;
 
-  /* Builds a map from <attr>=value to <attr>.<system>=value for each system.
+  /* Builds a map from <attr>=value to <attr>.<system>=value for each system,
+     while converting flake inputs from <attr>.<system>=value to <attr>=value
+
      See `mapDefaultSystems` for a full example
 
-     Type: mapSystems :: [string] -> (attrset -> attrset)
+     Type: mapSystems :: [string] -> (attrset -> attrset) -> attrset
 
      Example:
-       mapSystems ["x86_64-linux"] (pkgs: { hello = 42; })
+       mapSystems ["x86_64-linux"] ({ someAttrs }: { hello = someAttrs.packages.hello; }) { someAttrs = { packages.x86_64-linux.hello = 42; }}
        => { hello = { x86_64-linux = 42; }; }
   */
-  mapSystems = systems: f:
+  mapSystems = systems: f: inputs:
     let
       op = attrs: system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
-          ret = f pkgs;
+          flattenInput = builtins.mapAttrs (_: attrs: if attrs ? ${system} then attrs.${system} else attrs);
+          inputs' = builtins.mapAttrs (_: input: flattenInput input) inputs;
+          ret = f inputs';
           op = attrs: key:
             attrs //
             {
