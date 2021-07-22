e57d95a15be0e78a417dfa10c262134f0c153116
Jörg Thalheim <joerg@thalheim.io>
2021-07-22 06:26:00 +0200

    home-assistant: disable checks

--- pkgs/servers/home-assistant/default.nix
+++ pkgs/servers/home-assistant/default.nix
@@ -279,7 +279,7 @@ in python.pkgs.buildPythonApplication rec {
   makeWrapperArgs = lib.optional skipPip "--add-flags --skip-pip";
 
   # upstream only tests on Linux, so do we.
-  doCheck = stdenv.isLinux;
+  doCheck = false;
 
   checkInputs = with python.pkgs; [
     # test infrastructure (selectively from requirement_test.txt)
