cbf905a7583659fc6c6aad6569c12515b0dfad4a
Jörg Thalheim <joerg@thalheim.io>
2022-01-08 10:11:54 +0100

    firecracker: build from source

--- pkgs/applications/virtualization/firecracker/default.nix
+++ pkgs/applications/virtualization/firecracker/default.nix
@@ -1,49 +1,21 @@
-{ fetchurl, lib, stdenv }:
+{ pkgsMusl, fetchFromGitHub, lib }:
 
-let
-  version = "0.24.5";
-
-  suffix = {
-    x86_64-linux = "x86_64";
-    aarch64-linux = "aarch64";
-  }."${stdenv.hostPlatform.system}" or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
-
-  baseurl = "https://github.com/firecracker-microvm/firecracker/releases/download";
-
-  dlbin = sha256: fetchurl {
-    url = "${baseurl}/v${version}/firecracker-v${version}-${suffix}.tgz";
-    sha256 = sha256."${stdenv.hostPlatform.system}";
-  };
-
-in
-stdenv.mkDerivation {
+pkgsMusl.rustPlatform.buildRustPackage rec {
   pname = "firecracker";
-  inherit version;
+  version = "0.25.2";
 
-  sourceRoot = ".";
-  src = dlbin {
-    x86_64-linux = "sha256-drcm2kz2csuJqr8Oqs0r1BrxgPHOyuwC2S+99MhbMjA=";
-    aarch64-linux = "sha256-x8RoBmgY3HRUOLw8YzEwQfQuT83zGfBHHWu88b4i05o=";
+  src = fetchFromGitHub {
+    owner = "firecracker-microvm";
+    repo = "firecracker";
+    rev = "v${version}";
+    sha256 = "sha256-HoOfltEiUEOTMhVAT5ZmFUNV0qSNS/C303NUu8Y3HuI=";
   };
 
-  dontConfigure = true;
+  cargoHash = "sha256-IIu4ZKD2HLdfGlJooYOgrLiY1xDj+rC2r1XuYECGu5w=";
 
-  buildPhase = ''
-    mv release-v${version}/firecracker-v${version}-${suffix} firecracker
-    mv release-v${version}/jailer-v${version}-${suffix} jailer
-    chmod +x firecracker jailer
-  '';
-
-  doCheck = true;
-  checkPhase = ''
+  postCheck = ''
     ./firecracker --version
     ./jailer --version
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    install -D firecracker $out/bin/firecracker
-    install -D jailer      $out/bin/jailer
   '';
 
   meta = with lib; {
@@ -54,3 +26,51 @@ stdenv.mkDerivation {
     maintainers = with maintainers; [ thoughtpolice endocrimes ];
   };
 }
+
+#let
+#  version = "0.24.5";
+#
+#  suffix = {
+#    x86_64-linux = "x86_64";
+#    aarch64-linux = "aarch64";
+#  }."${stdenv.hostPlatform.system}" or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+#
+#  baseurl = "https://github.com/firecracker-microvm/firecracker/releases/download";
+#
+#  dlbin = sha256: fetchurl {
+#    url = "${baseurl}/v${version}/firecracker-v${version}-${suffix}.tgz";
+#    sha256 = sha256."${stdenv.hostPlatform.system}";
+#  };
+#
+#in
+#stdenv.mkDerivation {
+#  pname = "firecracker";
+#  inherit version;
+#
+#  sourceRoot = ".";
+#  src = dlbin {
+#    x86_64-linux = "sha256-drcm2kz2csuJqr8Oqs0r1BrxgPHOyuwC2S+99MhbMjA=";
+#    aarch64-linux = "sha256-x8RoBmgY3HRUOLw8YzEwQfQuT83zGfBHHWu88b4i05o=";
+#  };
+#
+#  dontConfigure = true;
+#
+#  buildPhase = ''
+#    mv release-v${version}/firecracker-v${version}-${suffix} firecracker
+#    mv release-v${version}/jailer-v${version}-${suffix} jailer
+#    chmod +x firecracker jailer
+#  '';
+#
+#  doCheck = true;
+#  checkPhase = ''
+#    ./firecracker --version
+#    ./jailer --version
+#  '';
+#
+#  installPhase = ''
+#    mkdir -p $out/bin
+#    install -D firecracker $out/bin/firecracker
+#    install -D jailer      $out/bin/jailer
+#  '';
+#
+#}
