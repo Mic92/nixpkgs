85a97ed0495ec6983c7a95696b05f45c729b769b
Jörg Thalheim <joerg@thalheim.io>
2022-02-09 14:17:24 +0100

    k0sctl: 0.11.4 -> 0.12.6

--- pkgs/applications/networking/cluster/k0sctl/default.nix
+++ pkgs/applications/networking/cluster/k0sctl/default.nix
@@ -1,20 +1,28 @@
 { lib
 , buildGoModule
 , fetchFromGitHub
+, fetchpatch
 }:
 
 buildGoModule rec {
   pname = "k0sctl";
-  version = "0.11.4";
+  version = "0.12.6";
 
   src = fetchFromGitHub {
     owner = "k0sproject";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-Fk1aYSa3LqzxiHtlzH5pcNtodOprjfnCFh4UMqCa6Rc=";
+    sha256 = "sha256-TkkMO6xBHY5t5Rpd0ieSDXMrnQ+Xdq+65Rk93ZkYcUs=";
   };
 
-  vendorSha256 = "sha256-21C6wZ8lKQnbUg3aD0ZFVOgopblXyWk4WP/ubZVk3Yk=";
+  vendorSha256 = "sha256-nTAuvHcsJiW0XYX5GM1SL8cnOhwdrj6iw8tuAkEWNzQ=";
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/k0sproject/k0sctl/commit/22c694ab0335a1e6146d0d3f939ef79d2c005a3d.patch";
+      sha256 = "sha256-Ftq/vbQd5ArdHboDt6NdyuqpFalHVnsQBdpmyDG/t5Q=";
+    })
+  ];
 
   ldflags = [
     "-s"
