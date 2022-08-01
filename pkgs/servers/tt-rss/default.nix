d27939310df2905fd01a1b955a01e8c0849d42a3
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-08-01 09:05:12 +0100

    tt-rss: 2021-06-21 -> 2022-08-01
    Pull in a year of upstream changes.
    This update will involve two database migrations when logging in to the
    resulting service.

--- pkgs/servers/tt-rss/default.nix
+++ pkgs/servers/tt-rss/default.nix
@@ -1,16 +1,13 @@
-{ lib, stdenv, fetchurl }:
+{ lib, stdenv, fetchgit }:
 
 stdenv.mkDerivation rec {
   pname = "tt-rss";
-  year = "21";
-  month = "09";
-  day = "02";
-  version = "20${year}-${month}-${day}";
-  rev = "b8f82ca12f2b445c2eb74f1f3d0c12cb33b8e87f";
+  version = "unstable-2022-08-01";
 
-  src = fetchurl {
-    url = "https://git.tt-rss.org/fox/tt-rss/archive/${rev}.tar.gz";
-    sha256 = "sha256-E5wNEryW+P/O3qMj4uj3Lbh7UqPi4R97sJCG1lqrrtw=";
+  src = fetchgit {
+    url = "https://git.tt-rss.org/fox/tt-rss.git";
+    rev = "ed2cbeffcc456a86726b52d37c977a35b895968c";
+    sha256 = "0ab1q316y4f432z2kwn86kc144awk529cild7b4jbffh2ydlj3r4";
   };
 
   installPhase = ''
@@ -21,7 +18,7 @@ stdenv.mkDerivation rec {
 
     # see the code of Config::get_version(). you can check that the version in
     # the footer of the preferences pages is not UNKNOWN
-    echo "${year}.${month}" > $out/version_static.txt
+    echo "22.08" > $out/version_static.txt
 
     runHook postInstall
   '';
