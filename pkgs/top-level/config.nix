d14a9f86465ca25ec403b2f8b7a7867a6332d30a
Jörg Thalheim <joerg@thalheim.io>
2022-09-07 10:38:18 +0200

    allow unfree packages by default
    <!-- ps-id: 5cc61b65-7ab9-4cd6-8a1d-ad2c98262c62 -->

--- pkgs/top-level/config.nix
+++ pkgs/top-level/config.nix
@@ -251,7 +251,7 @@ let
 
     allowUnfree = mkOption {
       type = types.bool;
-      default = false;
+      default = true;
       # getEnv part is in check-meta.nix
       defaultText = literalExpression ''false || builtins.getEnv "NIXPKGS_ALLOW_UNFREE" == "1"'';
       description = ''
@@ -555,5 +555,4 @@ in
       })
     ];
   };
-
 }
