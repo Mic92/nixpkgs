b1e96c007efbe25a76f2bec1fe5737aab8b74b9e
Jörg Thalheim <joerg@thalheim.io>
2022-09-07 10:38:18 +0200

    allow unfree packages by default

--- pkgs/top-level/config.nix
+++ pkgs/top-level/config.nix
@@ -79,7 +79,7 @@ let
 
     allowUnfree = mkOption {
       type = types.bool;
-      default = false;
+      default = true;
       # getEnv part is in check-meta.nix
       defaultText = literalExpression ''false || builtins.getEnv "NIXPKGS_ALLOW_UNFREE" == "1"'';
       description = lib.mdDoc ''
@@ -153,6 +153,8 @@ in {
     warnings = lib.optionals config.warnUndeclaredOptions (
       lib.mapAttrsToList (k: v: "undeclared Nixpkgs option set: config.${k}") config._undeclared or {}
     );
+
+    android_sdk.accept_license = true;
   };
 
 }
