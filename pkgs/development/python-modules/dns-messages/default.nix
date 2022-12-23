4cb3286f23355df3b80cd269dd24be116374f7ac
Guillaume Girol <symphorien+git@xlumurb.eu>
2022-12-23 12:00:00 +0000

    python3.pkgs.dns-messages: init at 1.0.0

---
+++ pkgs/development/python-modules/dns-messages/default.nix
@@ -0,0 +1,19 @@
+{ buildPythonPackage, fetchPypi, lib }:
+buildPythonPackage rec {
+  pname = "dns-messages";
+  version = "1.0.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-00gjYwEUeizid/kXxGV2WjBzO/PCpDjV2hMmYUdjPd4=";
+  };
+
+  doCheck = false;
+
+  meta = {
+    description = "A Python3 library for parsing and generating DNS messages";
+    homepage = "https://github.com/wahlflo/dns-messages";
+    license = [ lib.licenses.mit ];
+    maintainers = [ lib.maintainers.symphorien ];
+  };
+}
