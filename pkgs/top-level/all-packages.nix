1461c9e28e409e22a94250fa9271a604d529ae53
Guillaume Girol <symphorien+git@xlumurb.eu>
2022-12-23 12:00:00 +0000

    fakedns: init at unstable

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4492,6 +4492,8 @@ with pkgs;
 
   facter = callPackage ../tools/system/facter { };
 
+  fakedns = callPackage ../tools/networking/fakedns { };
+
   faketty = callPackage ../tools/misc/faketty { };
 
   fasd = callPackage ../tools/misc/fasd { };
