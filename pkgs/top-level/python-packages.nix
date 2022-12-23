9c291349e90f51b586c6342c9654ba312e0e7c4f
Guillaume Girol <symphorien+git@xlumurb.eu>
2022-12-23 12:00:00 +0000

    python3.pkgs.cli-formatter: init at 1.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1817,6 +1817,8 @@ self: super: with self; {
 
   clize = callPackage ../development/python-modules/clize { };
 
+  cli-formatter = callPackage ../development/python-modules/cli-formatter { };
+
   clldutils = callPackage ../development/python-modules/clldutils { };
 
   cloudflare = callPackage ../development/python-modules/cloudflare { };
