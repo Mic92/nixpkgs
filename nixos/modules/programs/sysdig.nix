eccc11047ae418c8ec514816f7a4d36c889c9a67
Jörg Thalheim <joerg@thalheim.io>
2023-05-19 07:20:39 +0200

    add sysdig option

--- nixos/modules/programs/sysdig.nix
+++ nixos/modules/programs/sysdig.nix
@@ -6,9 +6,10 @@ let
   cfg = config.programs.sysdig;
 in {
   options.programs.sysdig.enable = mkEnableOption (lib.mdDoc "sysdig");
+  options.programs.sysdig.package = lib.mkPackageOptionMD config.boot.kernelPackages "sysdig" { };
 
   config = mkIf cfg.enable {
     environment.systemPackages = [ pkgs.sysdig ];
-    boot.extraModulePackages = [ config.boot.kernelPackages.sysdig ];
+    boot.extraModulePackages = [ config.programs.sysdig.package ];
   };
 }
