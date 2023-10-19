cf068966bc311dcd946efe1eac889815640d264f
Jörg Thalheim <joerg@thalheim.io>
2023-10-19 14:50:04 +0100

    add license helper
    <!-- ps-id: e0f0e241-bf9e-447f-82ce-e9d35da574d6 -->

--- lib/default.nix
+++ lib/default.nix
@@ -599,6 +599,7 @@ let
         nixType
         imap
         ;
+      inherit (callLibs ./halalify.nix) halalify haramify;
       inherit (self.versions)
         compareVersions
         splitVersion
