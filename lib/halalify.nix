d95383f05d0313e8eb0fd7fd8b49c77ac147405e
Jörg Thalheim <joerg@thalheim.io>
2023-10-19 14:50:04 +0100

    add license helper

---
+++ lib/halalify.nix
@@ -0,0 +1,5 @@
+{ lib }:
+{
+  halalify = drv: drv.overrideAttrs (_old: { meta = _old.meta // { license = lib.licenses.free; }; });
+  haramify = drv: drv.overrideAttrs (_old: { meta = _old.meta // { license = lib.licenses.unfree; }; });
+}
