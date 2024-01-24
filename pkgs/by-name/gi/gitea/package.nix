23064f141d983e05c098c560571639e981de6558
Jörg Thalheim <joerg@thalheim.io>
2024-01-24 23:57:49 +0100

    gitea: add zstd patch
    <!-- ps-id: 05f98184-af4c-4f7d-a630-55d04ec7d067 -->

--- pkgs/by-name/gi/gitea/package.nix
+++ pkgs/by-name/gi/gitea/package.nix
@@ -71,7 +71,11 @@ buildGoModule (finalAttrs: {
     "data"
   ];
 
-  patches = [ ./static-root-path.patch ];
+  patches = [
+    # TODO: needs an update.
+    # ./0001-zstd-archive-support.patch
+    ./static-root-path.patch
+  ];
 
   # go-modules derivation doesn't provide $data
   # so we need to wait until it is built, and then
