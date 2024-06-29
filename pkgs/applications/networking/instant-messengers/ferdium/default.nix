e6b3709c73eb0a28a8e8acd30b36568a701b946e
Jörg Thalheim <joerg@thalheim.io>
2024-06-29 16:51:36 +0200

    ferdium: 6.7.4 -> 6.7.5
    Diff: https://github.com/ferdium/ferdium-app/compare/None...v6.7.5
    (cherry picked from commit c3d8b7d7fd7c32606d4eb6dce9875bf9cef63647)

--- pkgs/applications/networking/instant-messengers/ferdium/default.nix
+++ pkgs/applications/networking/instant-messengers/ferdium/default.nix
@@ -6,14 +6,14 @@ let
     aarch64-linux = "arm64";
   }."${stdenv.hostPlatform.system}" or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
   hash = {
-    amd64-linux_hash = "sha256-4yMaW1ByEQaQHdn99TGpUez2jVi5Ad4ODpugzW6kTm8=";
+    amd64-linux_hash = "sha256-PhWYHGRSX9wpPAlfA/UN2nmr++IWbeeEadgU6WAXaTQ=";
     arm64-linux_hash = "sha256-oIkHF1l7Un4W6/zufa+DOLi9oujQ2Zaq8AaKoHDAMRc=";
   }."${arch}-linux_hash";
 in
 mkFranzDerivation rec {
   pname = "ferdium";
   name = "Ferdium";
-  version = "6.7.4";
+  version = "6.7.5";
   src = fetchurl {
     url = "https://github.com/ferdium/ferdium-app/releases/download/v${version}/Ferdium-linux-${version}-${arch}.deb";
     inherit hash;
