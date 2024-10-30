f7f54f1815c85331ba14eea4e762e352930b2a87
Jörg Thalheim <joerg@thalheim.io>
2024-10-30 14:06:52 +0100

    Revert "matrix-sliding-sync: drop"
    This reverts commit 6306bf790e8e60854d76a3554031ce6e3347112f.
    
    <!-- ps-id: a4b2cd3b-4241-425b-9c07-2854cd9d4a29 -->

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -864,7 +864,8 @@ mapAliases {
   matomo_5 = matomo; # Added 2024-12-12
   matomo-beta = throw "matomo-beta has been removed as it mostly just pointed to the latest matomo release, use `matomo.overrideAttrs` to access a specific beta version instead"; # Added 2025-01-15
   matrique = throw "'matrique' has been renamed to/replaced by 'spectral'"; # Converted to throw 2024-10-17
-  matrix-sliding-sync = throw "matrix-sliding-sync has been removed as matrix-synapse 114.0 and later covers its functionality"; # Added 2024-10-20
+  matrixcli = throw "'matrixcli' has been removed due to being unmaintained and broken functionality. Recommend 'matrix-commander' as an alternative"; # Added 2024-03-09
+  matrix-recorder = throw "matrix-recorder has been removed due to being unmaintained"; # Added 2023-05-21
   maui-nota = libsForQt5.mauiPackages.nota; # added 2022-05-17
   maui-shell = throw "maui-shell has been removed from nixpkgs, it was broken"; # Added 2024-07-15
   mcomix3 = mcomix; # Added 2022-06-05
