a6d8ee6f66a725f9bc1264a84e0c011ea83627c7
Jörg Thalheim <joerg@thalheim.io>
2024-10-30 14:06:52 +0100

    Revert "matrix-sliding-sync: drop"
    This reverts commit 6306bf790e8e60854d76a3554031ce6e3347112f.
    
    <!-- ps-id: a4b2cd3b-4241-425b-9c07-2854cd9d4a29 -->

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3165,6 +3165,8 @@ with pkgs;
 
   maskromtool = qt6Packages.callPackage ../tools/graphics/maskromtool { };
 
+  matrix-sliding-sync = callPackage ../servers/matrix-synapse/sliding-sync { };
+
   matrix-synapse-plugins = recurseIntoAttrs matrix-synapse-unwrapped.plugins;
 
   maubot = with python3Packages; toPythonApplication maubot;
