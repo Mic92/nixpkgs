3e10ece5ab1802657c0774f6993c89ba7de4242b
Jörg Thalheim <joerg@thalheim.io>
2024-12-09 20:23:25 +0100

    harmonia: 1.0.2 -> 2.0.1

--- pkgs/by-name/ha/harmonia/package.nix
+++ pkgs/by-name/ha/harmonia/package.nix
@@ -13,16 +13,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "harmonia";
-  version = "1.0.2";
+  version = "2.0.1";
 
   src = fetchFromGitHub {
     owner = "nix-community";
     repo = "harmonia";
     rev = "refs/tags/harmonia-v${version}";
-    hash = "sha256-72nDVSvUfZsLa2HbyricOpA0Eb8gxs/VST25b6DNBpM=";
+    hash = "sha256-tqkTzUdwnTfVuCrcFag7YKgGkiR9srR45e4v0XMXVCY=";
   };
 
-  cargoHash = "sha256-gW/OljEngDQddIovtgwghu7uHLFVZHvWIijPgbOOkDc=";
+  cargoHash = "sha256-iZbIuYSC/RLmYEhBu46EEzN+WG2RoUJbZegedXkh+Rg=";
 
   doCheck = false;
 
