7e2df1cccfeabcc0333f2432b7600b879346a0dd
Jörg Thalheim <joerg@thalheim.io>
2024-12-15 14:14:10 +0100

    test

--- pkgs/by-name/he/hello/package.nix
+++ pkgs/by-name/he/hello/package.nix
@@ -13,6 +13,8 @@ stdenv.mkDerivation (finalAttrs: {
   pname = "hello";
   version = "2.12.1";
 
+  test = 1;
+
   src = fetchurl {
     url = "mirror://gnu/hello/hello-${finalAttrs.version}.tar.gz";
     hash = "sha256-jZkUKv2SV28wsM18tCqNxoCZmLxdYH2Idh9RLibH2yA=";
