7b5c21b1d81fb751d19040b807b8429549b9e031
Jörg Thalheim <joerg@thalheim.io>
2025-06-11 20:53:13 +0200

    qemu-vm: don't run fsck on 9p
    this will throw (harmless) warnings

--- nixos/modules/virtualisation/qemu-vm.nix
+++ nixos/modules/virtualisation/qemu-vm.nix
@@ -1410,6 +1410,7 @@ in
             "x-systemd.requires=modprobe@9pnet_virtio.service"
           ]
           ++ lib.optional (tag == "nix-store") "cache=${cfg.nixStore9pCache}";
+          value.noCheck = true;
         };
       in
       lib.mkMerge [
