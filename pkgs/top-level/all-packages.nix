7cca0085fda7ddb4a3281ade6af9c936fc6695b8
pancaek <20342389+pancaek@users.noreply.github.com>
2025-10-20 20:20:42 -0700

    carla: move to by-name/

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10838,8 +10838,6 @@ with pkgs;
     confd-calico
     ;
 
-  carla = libsForQt5.callPackage ../applications/audio/carla { };
-
   cb2bib = libsForQt5.callPackage ../applications/office/cb2bib { };
 
   cbconvert-gui = cbconvert.gui;
