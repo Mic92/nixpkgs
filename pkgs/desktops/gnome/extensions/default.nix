1c1540a1ff58d4e1ac624a5251fe5c7d48e20800
Jörg Thalheim <joerg@thalheim.io>
2026-02-05 15:15:43 +0100

    gnomeExtensions: remove broken extensions
    Remove pidgin-im-integration and window-corner-preview extensions which
    have been marked broken since NixOS 24.05 (gnome-shell >= 3.32) and
    remain unfixed. Both extensions haven't been updated for modern GNOME
    Shell versions.
    
    Per RFC 180, packages broken for a full release cycle are subject
    to removal.
    
    https://github.com/NixOS/rfcs/pull/180

--- pkgs/desktops/gnome/extensions/default.nix
+++ pkgs/desktops/gnome/extensions/default.nix
@@ -98,7 +98,9 @@ rec {
         icon-hider = throw "gnomeExtensions.icon-hider was removed on 2024-03-15. The extension has not received any updates since 2020/3.34.";
         nohotcorner = throw "gnomeExtensions.nohotcorner removed since 2019-10-09: Since 3.34, it is a part of GNOME Shell configurable through GNOME Tweaks.";
         mediaplayer = throw "gnomeExtensions.mediaplayer deprecated since 2019-09-23: retired upstream https://github.com/JasonLG1979/gnome-shell-extensions-mediaplayer/blob/master/README.md";
+        pidgin-im-integration = throw "gnomeExtensions.pidgin-im-integration has been removed due to being broken for more than a year; see RFC 180"; # Added 2026-02-05
         remove-dropdown-arrows = throw "gnomeExtensions.remove-dropdown-arrows removed since 2021-05-25: The extensions has not seen an update sine GNOME 3.34. Furthermore, the functionality it provides is obsolete as of GNOME 40.";
+        window-corner-preview = throw "gnomeExtensions.window-corner-preview has been removed due to being broken for more than a year; see RFC 180"; # Added 2026-02-05
       }
     )
     # Export buildShellExtension function
--- pkgs/desktops/gnome/extensions/manuallyPackaged.nix
+++ pkgs/desktops/gnome/extensions/manuallyPackaged.nix
@@ -10,7 +10,7 @@
   "guillotine@fopdoodle.net" = callPackage ./guillotine { };
   "impatience@gfxmonk.net" = callPackage ./impatience { };
   "no-title-bar@jonaspoehler.de" = callPackage ./no-title-bar { };
-  "pidgin@muffinmad" = callPackage ./pidgin-im-integration { };
+
   "pop-shell@system76.com" = callPackage ./pop-shell { };
   "sound-output-device-chooser@kgshank.net" = callPackage ./sound-output-device-chooser { };
   "systemd-manager@hardpixel.eu" = callPackage ./systemd-manager { };
@@ -20,7 +20,7 @@
   # Can be removed when https://github.com/hardpixel/unite-shell/issues/353 resolved
   "unite@hardpixel.eu" = callPackage ./unite { };
   "valent@andyholmes.ca" = callPackage ./valent { };
-  "window-corner-preview@fabiomereu.it" = callPackage ./window-corner-preview { };
+
   # Can be removed when https://github.com/oae/gnome-shell-pano/issues/271 resolved
   "pano@elhan.io" = callPackage ./pano { };
 }
